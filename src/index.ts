8ab40a10fe36ba6009ac6e743fcbec268b98574b
ZZJsky123 <1647372420@qq.com>
2026-04-18 16:27:11 +0800

    feat: finalize public library exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
+export { pumpBranch } from './branch-pump'
 export { pumpConfigDefaults } from './config'
+export { defaultDeps } from './default-deps'
 export { definePumpConfig } from './define-config'
-export { PumppError } from './errors'
+export { errorCodeToExit, PumppError, toPumppError } from './errors'
+export type { PumppErrorCode } from './errors'
+export { loadPumpConfig } from './load-pump-config'
+export { mergeTokenProviders, normalizePumpConfig } from './type-registry'
 export { ProgressEvent } from './type/pump-branch-progress'
-export type * from './type/pump-branch-progress'
-export type * from './type/pump-branch-results'
-export type * from './type/pump-config'
-export type * from './type/pump-deps'
-export type * from './type/pump-runtime-options'
-export type * from './type/token-provider'
+export type { PumpBranchProgress } from './type/pump-branch-progress'
+export type { PumpBranchResults } from './type/pump-branch-results'
+export type {
+  ManifestOptions,
+  PumpInputConfig,
+  ResolvedGlobals,
+  ResolvedPumpConfig,
+  ResolvedTypeConfig,
+  TypeInputConfig,
+} from './type/pump-config'
+export type { NameContext, PumpRuntimeOptions } from './type/pump-runtime-options'
+export type { GitDeps, PromptDeps, PumpDeps } from './type/pump-deps'
+export type { TokenContext, TokenProviderSpec } from './type/token-provider'
+export { buildBuiltinProviders, resolveTokens } from './utils/token-providers'
